7a85c4322520b40d1869d603014a2961e43c7c5b
poleshkkkaa <polinapolesko63@gmail.com>
2025-05-29 23:39:51 +0300

    Remove .env from Git tracking and update API

--- obj/Debug/net6.0/MotivationQuotesApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/MotivationQuotesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotivationQuotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521c598e09c475786ed0222160cc441d81280800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802a564c0e43f17b5bd00b30e1c1ec341cfc5efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotivationQuotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotivationQuotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
