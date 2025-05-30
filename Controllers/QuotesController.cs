ce997b035c63f470879f89445a63376c47106235
poleshkkkaa <polinapolesko63@gmail.com>
2025-05-30 20:33:29 +0300

    Update TimeSpan

--- Controllers/QuotesController.cs
+++ Controllers/QuotesController.cs
@@ -314,12 +314,11 @@ namespace MotivationQuotesAPI.Controllers
         {
             Console.WriteLine($"⏰ Час запиту: {time}");
 
-            if (!TimeSpan.TryParseExact(time, "HH\\:mm\\:ss", CultureInfo.InvariantCulture, out var parsedTime))
+            if (!TimeSpan.TryParseExact(time, @"hh\:mm\:ss", CultureInfo.InvariantCulture, out var parsedTime))
+
                 return BadRequest("❌ Неправильний формат часу");
 
-            var subscribers = await _dbContext.DailySubscribers
-                .Where(s => s.PreferredTime.Hours == parsedTime.Hours && s.PreferredTime.Minutes == parsedTime.Minutes)
-                .ToListAsync();
+            var subscribers = await _dbContext.DailySubscribers .Where(s => s.PreferredTime == parsedTime).ToListAsync();
 
             Console.WriteLine($"👥 Підписників знайдено: {subscribers.Count}");
 
