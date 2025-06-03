82f6a7b07ba05d0470af9e5125e8ed50f5f1b8c3
poleshkkkaa <polinapolesko63@gmail.com>
2025-06-03 17:05:29 +0300

    Apdate

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -6,9 +6,10 @@
         public string Text { get; set; } = "";
         public string Author { get; set; } = "";
         public bool IsFavorite { get; set; }
-        public long UserId { get; set; } 
+        public long UserId { get; set; }
         public int Likes { get; set; } = 0;
         public int Dislikes { get; set; } = 0;
     }
 }
+
 
